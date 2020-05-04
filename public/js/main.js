eecb21b04c430b6d0f6ea4921455bfefa817ab28
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-05-04 21:53:12 +0400

    wip: on responsive design

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
-$(document).ready(function () {
-  $(".homeSlider").slick({
+document.addEventListener('DOMContentLoaded', function () {
+  // Initizalie slick carousel
+  $('.homeSlider').slick({
     prevArrow: `<button type="button" title="Əvvəlki" class="homeSlider__prev">
                   <i aria-hidden="true" data-feather="chevron-left"></i>
                 </button>`,
@@ -8,5 +9,25 @@ $(document).ready(function () {
                 </button>`,
   });
 
+  // replace feather icons with actual SVG icon
   feather.replace();
+
+  // defer off-screen images
+  var lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+  if ('IntersectionObserver' in window) {
+    var lazyImageObserver = new IntersectionObserver(function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          var lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove('lazy');
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
 });
