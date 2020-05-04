fbe5a64f49ae09769a6dbfbbc5cd32a370998c0a
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-05-04 15:43:54 +0400

    updating img alternatives and link test

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
   $(".homeSlider").slick({
-    prevArrow:
-      '<button type="button" class="homeSlider__prev"><i data-feather="chevron-left"></i></button>',
-    nextArrow:
-      '<button type="button" class="homeSlider__next"><i data-feather="chevron-right"></i></button>',
+    prevArrow: `<button type="button" title="Əvvəlki" class="homeSlider__prev">
+                  <i aria-hidden="true" data-feather="chevron-left"></i>
+                </button>`,
+    nextArrow: `<button type="button" title="Sonrakı" class="homeSlider__next">
+                  <i aria-hidden="true" data-feather="chevron-right"></i>
+                </button>`,
   });
 
   feather.replace();
